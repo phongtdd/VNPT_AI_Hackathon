34f653046d535380a2f9df7d4755d8ba080e3684
Cr4nky04 <anh.nv225434@sis.hust.edu.vn>
2025-12-15 23:39:07 +0700

    feat(question_classify): add RAG question classification and CLI support

--- core/question_classify.py
+++ core/question_classify.py
@@ -6,7 +6,9 @@ from tqdm import tqdm
 from core.llm_interface import LLM_VNPTAI
 from prompt.agent_prompt import CLASSIFY_SYSTEM_PROMPT
 from utils.helper import classify_data, get_data, merge_by_qid
-
+from typing import List, Dict, Any, Callable, Optional
+import re
+import argparse
 
 def question_classify(dataset: list[dict[str, str]], llm: LLM_VNPTAI):
     question_str = ""
@@ -20,6 +22,21 @@ def question_classify(dataset: list[dict[str, str]], llm: LLM_VNPTAI):
     result = json.loads(response)
     return result
 
+def classify_rag(questions: List[Dict[str, Any]]):
+    RAG_PREFIX_PATTERN = re.compile(r"^\s*đoạn\s+thông\s+tin\s*:\s*", re.IGNORECASE)
+
+    rag_question = []
+    remain_question= []
+
+    for i, q in enumerate(questions):
+        q = q or ""
+        is_rag = bool(RAG_PREFIX_PATTERN.search(q["question"]))
+        if is_rag:
+            q["label"] = "RAG"
+            rag_question.append(q)
+        else:
+            remain_question.append(q)
+    return rag_question, remain_question
 
 def classify(
     data_path,
@@ -27,6 +44,8 @@ def classify(
     batch_size: int = 20,
 ):
     data = get_data(data_path)
+    rag_question, remain_question = classify_rag(data)
+
     llm = LLM_VNPTAI(
         llm_name=model_name,
         system_prompt=CLASSIFY_SYSTEM_PROMPT,
@@ -37,21 +56,21 @@ def classify(
     )
 
     BATCH_SIZE_CLASSIFY = batch_size
-    classified_results = []
+    remain_classified_results = []
 
-    total_batches = (len(data) + BATCH_SIZE_CLASSIFY - 1) // BATCH_SIZE_CLASSIFY
+    total_batches = (len(remain_question) + BATCH_SIZE_CLASSIFY - 1) // BATCH_SIZE_CLASSIFY
 
     for i in tqdm(
-        range(0, len(data), BATCH_SIZE_CLASSIFY),
+        range(0, len(remain_question), BATCH_SIZE_CLASSIFY),
         total=total_batches,
         desc="Classifying questions",
         unit="batch",
     ):
-        batch = data[i : i + BATCH_SIZE_CLASSIFY]
+        batch = remain_question[i : i + BATCH_SIZE_CLASSIFY]
         result = question_classify(batch, llm)
-        classified_results.extend(result)
-
-    classified_data = merge_by_qid(data, classified_results)
+        remain_classified_results.extend(result)
+    remain_classified_results.extend(rag_question)
+    classified_data = merge_by_qid(data, remain_classified_results)
     return classified_data
 
 
@@ -59,3 +78,15 @@ def seperate_data(data_path, output_dir="seperated_data", batch_size: int = 20):
     classified = classify(data_path, batch_size=batch_size)
     classify_data(classified, output_dir=output_dir)
     print(f"Data separated and saved to {output_dir}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_path", required=True, help="Path to test dataset JSON")
+    parser.add_argument("--output_dir", required=False, help="Path to save predictions")
+
+    args = parser.parse_args()
+
+    if args.output_dir:
+        seperate_data(args.data_path, args.output_dir)
+    else:
+        seperate_data(args.data_path)
